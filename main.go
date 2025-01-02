8229a80867e08c338baf4f5bd07ddfb422deb1a0
fulsep <57238803+fulsep@users.noreply.github.com>
2025-01-02 04:51:47 +0000

    feat: add new endpoint /api/v2/hello

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 func main() {
 	r := gin.Default()
 
+	r.GET("/api/v2/hello", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"success": true,
+			"message": "Hello from v2",
+		})
+	})
+
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"success": true,
